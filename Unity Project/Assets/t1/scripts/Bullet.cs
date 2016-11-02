0e61de5e2f455e9c7b78aa57a0defb6887f80a1b
unknown <dnl.rammer@gmail.com>
2016-11-02 08:50:45 +0100

    initial commit; tut 01 ready

---
+++ Unity Project/Assets/t1/scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 50;
+	public float lifeTime = 5;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Unity Project/Assets/t1/scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float speed = 0.5f;
+    public float rotationSpeed = 150;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, -speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			transform.Rotate (0, rotationSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate (0, -rotationSpeed * Time.deltaTime, 0);
+		}
+
+	}
+}
