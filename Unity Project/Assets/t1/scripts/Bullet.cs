a82df82869cafc7b31a899b3764106955c8967c6
unknown <dnl.rammer@gmail.com>
2016-11-03 08:14:20 +0100

    wip

--- Unity Project/Assets/t1/scripts/Bullet.cs
+++ Unity Project/Assets/t1/scripts/Bullet.cs
@@ -6,13 +6,18 @@ public class Bullet : MonoBehaviour {
 	public float speed = 50;
 	public float lifeTime = 5;
 
+    Rigidbody body;
+
 	// Use this for initialization
 	void Start () {
 		Destroy (this.gameObject, lifeTime);
-	}
+        body = GetComponent<Rigidbody>();
+
+        body.AddForce(transform.forward * speed, ForceMode.Impulse);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+        print("velo " + body.velocity.magnitude);
 	}
 }
--- Unity Project/Assets/t1/scripts/Gun.cs
+++ Unity Project/Assets/t1/scripts/Gun.cs
@@ -12,7 +12,7 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown(KeyCode.Space)) {
 			Instantiate (bulletPrefab, transform.position, transform.rotation);
 		}
 	}
