8238909b07775b3dac6ff0be61a799ece27ebb3d
Daniel Rammer <dnl.rammer@gmail.com>
2016-11-05 18:06:42 +0100

    after 2nd tutorial

---
+++ Unity Project/Assets/ChangeAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeAnimation : MonoBehaviour {
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.SetBool("Change", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
