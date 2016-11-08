288d628e0d8732b6609d659be2682af2816b29b5
unknown <dnl.rammer@gmail.com>
2016-11-08 12:12:51 +0100

    added menu

---
+++ Unity Project/Assets/t3_ui/MenuLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour {
+
+    public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape)) {
+            menu.SetActive(!menu.activeSelf);
+
+            if (!menu.activeSelf)
+                Time.timeScale = 1;
+            else
+                Time.timeScale = 0;
+        }
+	}
+
+    public void StartGame() {
+        SceneManager.LoadScene("animation");
+    }
+
+    public void GoToMenu() {
+        SceneManager.LoadScene("menu");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
